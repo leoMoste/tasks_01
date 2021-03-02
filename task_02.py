e5af1b9f8c175d9434531cbba373451542791955
leo <mostafahasan761@gmail.com>
2021-03-02 14:50:26 +0100

    the Solve of new Ex_02

---
+++ task_02.py
@@ -0,0 +1,15 @@
+
+
+
+
+def myrange(num):
+
+    if num < 20:
+        for x in range(num, 50 + 1 ,  3):
+            print(x)
+    elif num > 20:
+        for x in range(num, 50 + 1 , 2):
+            print(x)
+
+
+myrange(17)
