311ed2a2eaab6362437b15bef3e15be4f6de4ac7
leo <mostafahasan761@gmail.com>
2021-03-02 12:25:50 +0100

    first task in Python

---
+++ basics_2.py
@@ -0,0 +1,21 @@
+
+
+def sum(x,y):
+    print('func 1')
+    print(x+y)
+
+
+'''
+print(sum(2,3)*2) 
+that's not work becuse it's not return any value 
+'''
+
+x = sum(2,3)
+print(x)
+
+
+def sum2(x,y):
+   print('func 2')
+   return(x+y)
+
+print(sum2(2,3)*2)
---
+++ task_01.py
@@ -0,0 +1,9 @@
+
+
+def myrange(start, end):
+    for x in range(start + 1, end - 1):
+       print(x)
+
+
+
+myrange(5,20)
